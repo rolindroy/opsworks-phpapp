bdf86fdbc6b626fb7fae0aea872b052e3894ef00
Rolind Roy <rolind.roy@gmail.com>
2018-02-27 10:43:29 +0530

    added application recipe files

---
+++ application/recipes/default.rb
@@ -0,0 +1,17 @@
+# @Author: Rolind Roy <rolindroy>
+# @Date:   2018-02-27T10:31:25+05:30
+# @Email:  rolind.roy@tatacommunications.com
+# @Filename: default.rb
+# @Last modified by:   rolindroy
+# @Last modified time: 2018-02-27T10:42:52+05:30
+
+
+cookbook_file "/tmp/" do
+  source "isocrats.com.zip"
+  mode "0644"
+end
+
+execute 'extract_some_tar' do
+  command 'tar xzvf /tmp/isocrats.com.tar'
+  cwd '/var/www/html/'
+end
